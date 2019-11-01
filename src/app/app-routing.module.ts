4ed59572c407c8da9b74099fbde7c53e63ed4485
= <=>
2019-11-01 14:37:47 -0300

    Terminando footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoardComponent } from './components/board/board.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { EditCustomerComponent } from './components/edit-customer/edit-customer.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BoardComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'configuration', component: ConfigurationComponent},
+  { path: 'customer/edit/:id', component: EditCustomerComponent},
+  { path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
